a1e2fc479190510f1a51e79a6e8af513b8e587dd
Wayne <wdavis2002@gmail.com>
2019-08-09 20:46:58 -0500

    Add files via upload

---
+++ ITestRequests.cs
@@ -0,0 +1,52 @@
+﻿using FireManager.Entities.MemberAggregate;
+using FireManager.Entities.PositionAggregate;
+using FireManager.Entities.ScheduleAggregate;
+using FireManager.Entities.StaffedPositionAggregate;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FireManager.Tests
+{
+    public interface ITestRequests
+    {
+        /// <summary>
+        /// Calls all test methods and returns whether the test completed or not
+        /// </summary>
+        /// <returns>bool value indicating all tests were completed</returns>
+        Task<bool> RunTestSuite();
+
+        /// <summary>
+        /// Test method for Members
+        /// </summary>
+        /// <returns>List of FireManager Members</returns>
+        Task<IList<FireManagerMember>> TestMemberRequest();
+
+        /// <summary>
+        /// Test method for Schedules
+        /// </summary>
+        /// <returns>List of FireManager Schedules</returns>
+        Task<IList<FireManagerSchedule>> TestScheduleRequest();
+
+        /// <summary>
+        /// Test method for Positions
+        /// </summary>
+        /// <returns>List of FireManager Positions</returns>
+        Task<IList<FireManagerPosition>> TestPositionRequest();
+
+        /// <summary>
+        /// Test method for Staffed Positions
+        /// </summary>
+        /// <param name="Date">Specified date of staffed schedules</param>
+        /// <returns>List of Staffed Positions for the specified date</returns>
+        Task<IList<FireManagerStaffedPosition>> TestStaffedPositionRequest(DateTime Date);
+
+        /// <summary>
+        /// Test method for Staffed Positions
+        /// </summary>
+        /// <param name="Year">Specified year of staffed schedules</param>
+        /// <param name="Month">Specified month of staffed schedules</param>
+        /// <returns>List of Staffed Positions for the specified year and month</returns>
+        Task<IList<FireManagerStaffedPosition>> TestStaffedPositionRequest(int Year, int Month);
+    }
+}
